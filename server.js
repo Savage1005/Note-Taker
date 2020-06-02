47d1c7f6f938edde0dd693611973652766a3a908
Tom Savage <tsavage@gmail.com>
2020-06-02 00:12:34 -0400

    initial link

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app= express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname,'./public')))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
